d3adba23a16749de61bc054ccb586a90c6dde844
Ethan Nitz <etnitz@gmail.com>
2022-03-08 13:54:37 -0600

    working on logic

---
+++ script.js
@@ -0,0 +1,94 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const cardArray = [
+        {
+            name: 'bee',
+            img: 'images/bee.png'
+        },
+        {
+            name: 'bee',
+            img: 'images/bee.png'
+        },
+        {
+            name: 'bird',
+            img: 'images/bird.png'
+        },
+        {
+            name: 'bird',
+            img: 'images/bird.png'
+        },
+        {
+            name: 'cat',
+            img: 'images/cat.png'
+        },
+        {
+            name: 'cat',
+            img: 'images/cat.png'
+        },
+        {
+            name: 'cow',
+            img: 'images/cow.png'
+        },
+        {
+            name: 'cow',
+            img: 'images/cow.png'
+        },
+        {
+            name: 'deer',
+            img: 'images/deer.png'
+        },
+        {
+            name: 'deer',
+            img: 'images/deer.png'
+        },
+        {
+            name: 'dog',
+            img: 'images/dog.png'
+        },
+        {
+            name: 'dog',
+            img: 'images/dog.png'
+        },
+        {
+            name: 'dolphin',
+            img: 'images/dolphin.png'
+        },
+        {
+            name: 'dolphin',
+            img: 'images/dolphin.png'
+        },
+        {
+            name: 'elephant',
+            img: 'images/elephant.png'
+        },
+        {
+            name: 'elephant',
+            img: 'images/elephant.png'
+        }
+    ]
+
+    const gameBoard = document.querySelector('.game-board')
+    let cardsChosen = []
+    let cardsChosenId = []
+
+    function createBoard() {
+        for (i = 0; i < cardArray.length; i++) {
+            let card = document.createElement('img')
+            card.setAttribute('src', 'images/back.jpeg')
+            card.setAttribute('data-id', i)
+            card.addEventListener('click', flipCard)
+            gameBoard.appendChild(card)
+        }
+    }
+
+    function flipCard() {
+        let cardId = this.getAttribute('data-id')
+        cardsChosen.push(cardArray[cardId].name)
+        cardsChosenId.push(cardId)
+        this.setAttribute('src', cardArray[cardId].img)
+        if (cardsChosen.length === 2) {
+            setTimeout(checkForMatch, 500)
+        }
+    } 
+
+    createBoard()
+})

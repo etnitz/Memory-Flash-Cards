765cb43a43f13d90554dbc7010c81102a5206091
Ethan Nitz <etnitz@gmail.com>
2022-03-09 10:13:16 -0600

    created match logic

--- script.js
+++ script.js
@@ -68,20 +68,43 @@ document.addEventListener('DOMContentLoaded', () => {
     ]
 
     const board = document.querySelector('.game-board')
+    let cardsClicked = []
+    let cardsClickedId = []
 
 
 
-    function newBoard() {
+    const board = () => {
         cardArr.forEach( (i) => {
             let card = document.createElement('img')
             card.setAttribute('src', 'images/back.jpeg')
             card.setAttribute('card-id', i)
-
+            card.addEventListener('click', flip)
             board.appendChild(card)
         })
     }
 
-    
+    const match = () => {
+        let cards = document.querySelector('img')
+        const cardOne = cardsClicked[0]
+        const cardTwo = cardsClicked[1]
+        if (cardsClicked[0] === cardsClicked[1]) {
+            cards[cardOne].setAttribute('src', 'images/white.jpeg')
+            cards[cardTwo].setAttribute('src', 'images/white.jpeg')
+        } else {
+            cards[cardOne].setAttribute('src', 'images/back.jpeg')
+            cards[cardTwo].setAttribute('src', 'images/back.jpeg')
+        }
+    }
 
-    newBoard()
+    const flip = () => {
+        let cardId = this.getAttribute('card-id')
+        cardsClicked.push(cardArr[cardId].name)
+        cardsClickedId.push(cardId)
+        this.setAttribute('src', cardArr[cardId].img)
+        if (cardsClicked.length === 2) {
+            setTimeout(match, 500)
+        }
+    }
+
+    board()
 })

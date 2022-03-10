ef675840113d2f479dc82498608e47c9f3f3d988
Ethan Nitz <etnitz@gmail.com>
2022-03-10 10:25:08 -0600

    fixed some bugs

--- script.js
+++ script.js
@@ -68,13 +68,12 @@ document.addEventListener('DOMContentLoaded', () => {
     ]
 
     cardArr.sort( () => 0.5 - Math.random())
+    
 
     const board = document.querySelector('.game-board')
-    let cardsClicked = []
     let cardsClickedId = []
     let cardsMatched = []
     
-
     const newBoard = () => {
         cardArr.forEach( (i) => {
             let card = document.createElement('img')
@@ -87,12 +86,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const match = () => {
         let cards = document.querySelector('img')
-        const cardOne = cardsClicked[0]
-        const cardTwo = cardsClicked[1]
-        if (cardsClicked[0] === cardsClicked[1]) {
+        const cardOne = cardsClickedId[0]
+        const cardTwo = cardsClickedId[1]
+        if (cardsClickedId[0] === cardsClickedId[1]) {
             cards[cardOne].setAttribute('src', 'images/check.png')
             cards[cardTwo].setAttribute('src', 'images/check.png')
-            cardsMatched.push(cardsClicked)
+            cardsMatched.push(cardsClickedId)
         } else {
             cards[cardOne].setAttribute('src', 'images/back.jpeg')
             cards[cardTwo].setAttribute('src', 'images/back.jpeg')
@@ -101,10 +100,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const flip = ()  => {
         let cardId = event.currentTarget.getAttribute('card-id')
-        cardsClicked.push(cardArr[cardId])
         cardsClickedId.push(cardId)
         event.currentTarget.setAttribute('src', cardId)
-        if (cardsClicked.length === 2) {
+        if (cardsClickedId.length === 2) {
             setTimeout(match, 500)
         }
     }
